5e6183bfaa31937604e89f71ac6400c7b7e3c1a0
Syam Sundar K <k.syamsundar21@gmail.com>
2018-10-17 17:58:43 +0530

    login update

--- app/src/main/java/com/bugscript/slackup/Utilities/Constants.kt
+++ app/src/main/java/com/bugscript/slackup/Utilities/Constants.kt
@@ -4,7 +4,8 @@ const val BASE_URL_CONSTANT = "https://slack-up.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL_CONSTANT}account/register"
 const val URL_LOGIN = "${BASE_URL_CONSTANT}account/login"
 const val URL_CREATE_USER = "${BASE_URL_CONSTANT}user/add"
+const val URL_GET_USER = "${BASE_URL_CONSTANT}user/byEmail/"
 
 
-
+// Broadcast constant
 const val BROADCAST_USER_DATA_CHNAGE = "BROADCAST_USER_DATA_CHNAGE"
